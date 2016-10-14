ff63b40584fea89226352170f23fc2c4a6152f53
stuck <stuck@DESKTOP-UKI36E7>
2016-10-13 18:15:04 -0700

    Add project files.

---
+++ SchedulingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchedulingSystem.Startup))]
+namespace SchedulingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
