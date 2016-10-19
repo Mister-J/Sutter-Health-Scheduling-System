d180c49992bbcea58edba379135f0b1c7c412218
Mister-J <stuckonbasketball@gmail.com>
2016-10-19 12:35:46 -0700

    Login Page and Main Page working without authentication

---
+++ SchedulingSystem/ConnectToLdap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.DirectoryServices;
+
+
+namespace SchedulingSystem
+{
+    public class ConnectToLdap
+    {
+        private static string username;
+        private static string password;
+
+        public ConnectToLdap(string usernameText, string passwordText)
+        {
+            username = usernameText;
+            password = passwordText;
+        }
+
+        public static DirectoryEntry createDirectoryEntry()
+        {
+            DirectoryEntry ldapConnection = new DirectoryEntry("LDAP://example.com", username, password);
+            ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
+            return ldapConnection; 
+        }
+
+    }
+
+}
